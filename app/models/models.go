838808af5b00c5edda073cc7cc340f6dbc25eb8e
Jinzhu <wosmvp@gmail.com>
2015-07-27 10:26:23 +0800

    Use GetSizes for book's cover images

--- app/models/models.go
+++ app/models/models.go
@@ -1,14 +1,10 @@
 package models
 
 import (
-	"log"
-	"time"
-
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
 	"github.com/qor/qor/l10n"
-	"github.com/qor/qor/media_library"
 	"github.com/qor/qor/publish"
 )
 
@@ -19,30 +15,6 @@ type Author struct {
 
 	Name string
 }
-
-type Book struct {
-	gorm.Model
-	publish.Status
-	l10n.Locale
-
-	Title       string
-	Synopsis    string
-	ReleaseDate time.Time
-	Authors     []*Author `gorm:"many2many:book_authors"`
-	Price       float64
-	CoverImage  media_library.FileSystem
-	// later
-	// CoverImages []ProductImage // product image has BookId => handles relation
-}
-
-// type ProductImage struct {
-// 	gorm.Model
-// 	publish.Status
-// 	l10n.Locale
-
-// 	BookId     uint
-// 	CoverImage media_library.FileSystem
-// }
 
 type User struct {
 	gorm.Model
@@ -61,12 +33,7 @@ func (User) ViewableLocales() []string {
 
 func (user User) EditableLocales() []string {
 	if user.Role == "admin" {
-		log.Println("EditableLocales() admin")
 		return []string{l10n.Global, "ja-JP"}
-		// return []string{l10n.Global}
-	} else {
-		//log.Println("EditableLocales() NOT admin")
-		//return []string{l10n.Global, "ja-JP"}
-		return []string{}
 	}
+	return []string{}
 }
