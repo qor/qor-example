2318488f3ccf17a87029564168b8eebf947ef4a5
Jinzhu <wosmvp@gmail.com>
2015-09-02 18:45:52 +0800

    Setup promotion rules/benefits

--- config/admin/enterprise.go
+++ config/admin/enterprise.go
@@ -9,6 +9,40 @@ import (
 )
 
 func init() {
+	type discountRateArgument struct {
+		Value uint
+	}
+	promotion.RegisterBenefitHandler(promotion.BenefitHandler{
+		Name:     "Discount Rate",
+		Resource: Admin.NewResource(&discountRateArgument{}),
+	})
+
+	type discountValueArgument struct {
+		Value float32
+	}
+	promotion.RegisterBenefitHandler(promotion.BenefitHandler{
+		Name:     "Discount Value",
+		Resource: Admin.NewResource(&discountValueArgument{}),
+	})
+
+	type amountArgument struct {
+		Amount int
+	}
+	promotion.RegisterRuleHandler(promotion.RuleHandler{
+		Name:     "Amount Greater Than",
+		Resource: Admin.NewResource(&amountArgument{}),
+	})
+
+	type userGroupArgument struct {
+		Group string
+	}
+	userGroupResource := Admin.NewResource(&userGroupArgument{})
+	userGroupResource.Meta(&admin.Meta{Name: "Group", Type: "select_one", Collection: []string{"VIP", "Employee", "Normal"}})
+	promotion.RegisterRuleHandler(promotion.RuleHandler{
+		Name:     "User Group",
+		Resource: userGroupResource,
+	})
+
 	promotion.AutoMigrate(db.DB)
 	Admin.AddResource(&promotion.PromotionDiscount{}, &admin.Config{Name: "Promotions", Menu: []string{"Site Management"}})
 }
