f245e4f930ec3dec35ca5bf4f879bc0270ca68a8
Jinzhu <wosmvp@gmail.com>
2015-09-02 13:58:08 +0800

    Remove unused files

--- db/db.go
+++ db/db.go
@@ -4,40 +4,23 @@ import (
 	"os"
 
 	"github.com/jinzhu/gorm"
-	"github.com/qor/qor-example/app/models"
-	"github.com/qor/qor/l10n"
-	"github.com/qor/qor/publish"
 )
 
-var (
-	DB           gorm.DB
-	Pub          *publish.Publish
-	ProductionDB *gorm.DB
-	StagingDB    *gorm.DB
-)
+var DB *gorm.DB
 
 func init() {
+	var db gorm.DB
 	var err error
 
 	if os.Getenv("DB") == "mysql" {
-		if DB, err = gorm.Open("mysql", "qor:qor@/qor_bookstore?parseTime=True&loc=Local"); err != nil {
+		if db, err = gorm.Open("mysql", "qor:qor@/qor_bookstore?parseTime=True&loc=Local"); err != nil {
 			panic(err)
 		}
 	} else {
-		if DB, err = gorm.Open("postgres", "user=qor password=qor dbname=qor_bookstore sslmode=disable"); err != nil {
+		if db, err = gorm.Open("postgres", "user=qor password=qor dbname=qor_bookstore sslmode=disable"); err != nil {
 			panic(err)
 		}
 	}
 
-	DB.AutoMigrate(&models.Author{}, &models.Book{}, &models.User{})
-	DB.LogMode(true)
-
-	Pub = publish.New(&DB)
-	Pub.AutoMigrate(&models.Author{}, &models.Book{})
-
-	StagingDB = Pub.DraftDB()         // Draft resources are saved here
-	ProductionDB = Pub.ProductionDB() // Published resources are saved here
-
-	l10n.Global = "en-US"
-	l10n.RegisterCallbacks(&DB)
+	DB = &db
 }
--- main.go
+++ main.go
@@ -1,41 +1,10 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-
-	"github.com/qor/qor-example/app/controllers"
-	. "github.com/qor/qor-example/app/resources"
-)
+import "net/http"
 
 func main() {
 	mux := http.NewServeMux()
-	Admin.MountTo("/admin", mux)
-
-	// frontend routes
-	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
-
-	// serve static files
-	router.StaticFS("/system/", http.Dir("public/system"))
-	router.StaticFS("/assets/", http.Dir("public/assets"))
-
-	// books
-	bookRoutes := router.Group("/books")
-	{
-		// listing
-		bookRoutes.GET("", controllers.ListBooksHandler)
-		bookRoutes.GET("/", controllers.ListBooksHandler) // really? i need both of those?...
-		// single book - product page
-		bookRoutes.GET("/:id", controllers.ViewBookHandler)
-	}
-
-	mux.Handle("/", router)
-
-	// handle login and logout of users
-	mux.HandleFunc("/login", controllers.LoginHandler)
-	mux.HandleFunc("/logout", controllers.LogoutHandler)
+	// Admin.MountTo("/admin", mux)
 
 	// start the server
 	http.ListenAndServe(":9000", mux)
