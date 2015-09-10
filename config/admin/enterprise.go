eb2272078641cc260e58a948ea310680c1a06a17
Jinzhu <wosmvp@gmail.com>
2015-09-10 13:52:32 +0800

    Add more promotion types

--- config/admin/enterprise.go
+++ config/admin/enterprise.go
@@ -9,22 +9,34 @@ import (
 )
 
 func init() {
+	// Benefits Definations
 	type discountRateArgument struct {
-		Value uint
+		Percentage uint
 	}
+	discountRateArgumentResource := Admin.NewResource(&discountRateArgument{})
+	discountRateArgumentResource.Meta(&admin.Meta{Name: "Percentage", Label: "Percentage (e.g enter 10 for a 10% discount)"})
+
 	promotion.RegisterBenefitHandler(promotion.BenefitHandler{
 		Name:     "Discount Rate",
-		Resource: Admin.NewResource(&discountRateArgument{}),
+		Resource: discountRateArgumentResource,
 	})
 
-	type discountValueArgument struct {
-		Value float32
+	type discountAmountArgument struct {
+		Amount float32
 	}
+	discountAmountArgumentResource := Admin.NewResource(&discountAmountArgument{})
+	discountAmountArgumentResource.Meta(&admin.Meta{Name: "Amount", Label: "Amount (e.g enter 10 for a 10$ discount)"})
+
 	promotion.RegisterBenefitHandler(promotion.BenefitHandler{
-		Name:     "Discount Value",
-		Resource: Admin.NewResource(&discountValueArgument{}),
+		Name:     "Discount Amount",
+		Resource: discountAmountArgumentResource,
 	})
 
+	promotion.RegisterBenefitHandler(promotion.BenefitHandler{
+		Name: "Free Shipping",
+	})
+
+	// Rules Definations
 	type amountArgument struct {
 		Amount int
 	}
@@ -41,6 +53,15 @@ func init() {
 	promotion.RegisterRuleHandler(promotion.RuleHandler{
 		Name:     "User Group",
 		Resource: userGroupResource,
+	})
+
+	type hasProductArgument struct {
+		ProductCode string
+	}
+	hasProductResource := Admin.NewResource(&hasProductArgument{})
+	promotion.RegisterRuleHandler(promotion.RuleHandler{
+		Name:     "Has Product",
+		Resource: hasProductResource,
 	})
 
 	promotion.AutoMigrate(db.DB)
