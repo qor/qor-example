67a8160924c9f4daa863337c3ecb17340e086c33
Jinzhu <wosmvp@gmail.com>
2015-09-16 11:52:58 +0800

    Show order's ShippingAddress, BillingAddress as single edit

--- config/admin/admin.go
+++ config/admin/admin.go
@@ -40,10 +40,11 @@ func init() {
 	Admin.AddResource(&models.Size{}, &admin.Config{Menu: []string{"Product Management"}})
 	Admin.AddResource(&models.Category{}, &admin.Config{Menu: []string{"Product Management"}})
 
-	Admin.AddResource(&models.Order{}, &admin.Config{Menu: []string{"Order Management"}})
+	order := Admin.AddResource(&models.Order{}, &admin.Config{Menu: []string{"Order Management"}})
+	order.Meta(&admin.Meta{Name: "ShippingAddress", Type: "single_edit"})
+	order.Meta(&admin.Meta{Name: "BillingAddress", Type: "single_edit"})
 
 	store := Admin.AddResource(&models.Store{}, &admin.Config{Menu: []string{"Store Management"}})
-	store.IndexAttrs("-Latitude", "-Longitude")
 	store.AddValidator(func(record interface{}, metaValues *resource.MetaValues, context *qor.Context) error {
 		if meta := metaValues.Get("Name"); meta != nil {
 			if name := utils.ToString(meta.Value); strings.TrimSpace(name) == "" {
