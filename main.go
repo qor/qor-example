f1e6ffa6fe3f668b324d8e45788e763747de121e
Jinzhu <wosmvp@gmail.com>
2015-09-16 16:13:40 +0800

    Serve more static files

--- main.go
+++ main.go
@@ -13,7 +13,9 @@ func main() {
 	mux := http.NewServeMux()
 	admin.Admin.MountTo("/admin", mux)
 
-	mux.Handle("/system/", http.FileServer(http.Dir("public")))
+	for _, path := range []string{"system", "javascripts", "stylesheets", "images"} {
+		mux.Handle(fmt.Sprintf("/%s/", path), http.FileServer(http.Dir("public")))
+	}
 
 	fmt.Printf("Listening on: %v\n", config.Config.Port)
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), mux); err != nil {
