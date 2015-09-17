f0a47a47324ed3d195f4a20f8659e213bfb32af1
Azuma Fan <azuma@theplant.jp>
2015-09-17 17:57:23 +0800

    add time filter method

--- config/admin/funcmap.go
+++ config/admin/funcmap.go
@@ -8,7 +8,6 @@ import (
 )
 
 func initFuncMap() {
-	Admin.RegisterFuncMap("last_week_label", lastWeekLabelData)
 	Admin.RegisterFuncMap("last_week_orders", lastWeekOrderData)
 	Admin.RegisterFuncMap("last_week_users", lastWeekUserData)
 }
@@ -18,21 +17,30 @@ type ChartData struct {
 	Date  time.Time
 }
 
-func lastWeekLabelData() (res []string) {
-	var dateLabel string
-	for i := 0; i < 7; i++ {
-		dateLabel = now.BeginningOfDay().AddDate(0, 0, -i).Format("Jan 2")
-		res = append([]string{dateLabel}, res...)
+/*
+date format 2015-01-23
+*/
+func GetChartData(table, start, end string) (res []ChartData) {
+	startdate, err := time.Parse("2006-01-02", start)
+	if err != nil {
+		return
 	}
+	enddate, err := time.Parse("2006-01-02", end)
+	if err != nil || enddate.UnixNano() < startdate.UnixNano() {
+		enddate = now.EndOfDay()
+	} else {
+		enddate.AddDate(0, 0, 1)
+	}
+	db.DB.Table(table).Where("created_at > ? AND created_at < ?", startdate, enddate).Select("date(created_at) as date, count(1) as total").Group("date(created_at)").Scan(&res)
 	return
 }
 
 func lastWeekOrderData() (res []ChartData) {
-	db.DB.Table("orders").Select("date(created_at) as date, count(1) as total").Group("date(created_at)").Scan(&res)
+	res = GetChartData("orders", time.Now().AddDate(0, 0, -6).Format("2006-01-02"), time.Now().Format("2006-01-02"))
 	return
 }
 
 func lastWeekUserData() (res []ChartData) {
-	db.DB.Table("users").Select("date(created_at) as date, count(1) as total").Group("date(created_at)").Scan(&res)
+	res = GetChartData("users", time.Now().AddDate(0, 0, -6).Format("2006-01-02"), time.Now().Format("2006-01-02"))
 	return
 }
