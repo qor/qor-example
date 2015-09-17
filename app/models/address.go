78af2092ed68e00df6daf53c9415564c1f3f8816
Hui <junhuif@gmail.com>
2015-09-17 18:07:36 +0800

    Format address value in index page for order

--- app/models/address.go
+++ app/models/address.go
@@ -17,5 +17,5 @@ type Address struct {
 }
 
 func (address Address) Stringify() string {
-	return fmt.Sprintf("%v, %v, %v", address.Address2, address.Address1, address.City)
+	return fmt.Sprintf("%s, %s", address.Address1, address.Address2)
 }
