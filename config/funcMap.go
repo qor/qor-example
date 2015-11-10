c332ffe42ebfb993e7b42aa0d207410ef8fe847e
binku <binku87@gmail.com>
2015-11-10 17:33:54 +0800

    I18n: add inline edit

--- config/funcMap.go
+++ config/funcMap.go
@@ -1,13 +1,19 @@
 package config
 
 import (
+	"github.com/qor/qor/i18n"
 	"html/template"
 )
 
 var FuncMap = template.FuncMap{
+	"renderInlineEditAssets": RenderInlineEditAssets,
 	"t": T,
 }
 
 func T(key string, value string, args ...interface{}) template.HTML {
-	return Config.I18n.Default(value).T("en-US", key, args)
+	return Config.I18n.EnableInlineEdit(true).Default(value).T("en-US", key, args)
+}
+
+func RenderInlineEditAssets() (template.HTML, error) {
+	return i18n.RenderInlineEditAssets(true, true)
 }
