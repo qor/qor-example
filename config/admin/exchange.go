80e1e46b700b43cfcfe0ad8dd61063145f083bb2
Jinzhu <wosmvp@gmail.com>
2015-12-15 16:57:07 +0800

    Add validations for exchange

--- config/admin/exchange.go
+++ config/admin/exchange.go
@@ -2,7 +2,11 @@ package admin
 
 import (
 	"github.com/qor/exchange"
+	"github.com/qor/qor"
 	"github.com/qor/qor-example/app/models"
+	"github.com/qor/qor/resource"
+	"github.com/qor/qor/utils"
+	"github.com/qor/qor/validations"
 )
 
 var ProductExchange *exchange.Resource
@@ -12,4 +16,11 @@ func init() {
 	ProductExchange.Meta(exchange.Meta{Name: "Code"})
 	ProductExchange.Meta(exchange.Meta{Name: "Name"})
 	ProductExchange.Meta(exchange.Meta{Name: "Price"})
+
+	ProductExchange.AddValidator(func(record interface{}, metaValues *resource.MetaValues, context *qor.Context) error {
+		if utils.ToInt(metaValues.Get("Price").Value) < 100 {
+			return validations.NewError(record, "Price", "price don't less than 100")
+		}
+		return nil
+	})
 }
