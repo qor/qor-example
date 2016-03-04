d618440ac9c78b20a44a2372ec6a9991a36b47ae
Jinzhu <wosmvp@gmail.com>
2016-03-04 18:31:16 +0800

    Remove uncessary codes from qor-example

--- config/admin/funcmap.go
+++ config/admin/funcmap.go
@@ -1,28 +1,12 @@
 package admin
 
-import (
-	"time"
-
-	"github.com/qor/qor-example/app/models"
-)
+import "github.com/qor/qor-example/app/models"
 
 func initFuncMap() {
 	Admin.RegisterFuncMap("latest_orders", latestOrders)
-	// Admin.RegisterFuncMap("last_week_orders_chart", lastWeekOrderChart)
-	// Admin.RegisterFuncMap("last_week_users_chart", lastWeekUserChart)
 }
 
 func latestOrders() (orders []models.Order) {
 	Admin.Config.DB.Order("id desc").Limit(5).Find(&orders)
-	return
-}
-
-func lastWeekOrderChart() (res []models.Chart) {
-	res = models.GetChartData("orders", time.Now().AddDate(0, 0, -6).Format("2006-01-02"), time.Now().Format("2006-01-02"))
-	return
-}
-
-func lastWeekUserChart() (res []models.Chart) {
-	res = models.GetChartData("users", time.Now().AddDate(0, 0, -6).Format("2006-01-02"), time.Now().Format("2006-01-02"))
 	return
 }
