e706687ee679d4e97e082c3b523e74c2f59ae868
Oleg Dolya <oleg.dolya@gmail.com>
2016-03-12 10:40:14 +0200

    renamed:    app/models/log_login.go -> app/models/auth.go

--- app/controllers/auth.go
+++ app/controllers/auth.go
@@ -10,40 +10,45 @@ import (
 	"github.com/qor/qor-example/db"
 )
 
+// curl -i -X POST -H "Content-Type: application/json" -d "{ \"username\": \"pos0001\", \"password\": \"123456\", \"metod\": \"password\" }" http://localhost:7000/api/v1/auth/0
 func LoginApi(ctx *gin.Context) {
 	var auth models.Auth
 	var currentUser models.User
 	if ctx.BindJSON(&auth) == nil {
-
-		if !db.DB.Where("password = ?", auth.Password).First(&currentUser).RecordNotFound() {
-			// t := time.NowLoginAt: time.Now,
-			t := time.Now()
-			login := models.LogLogin{
-				ClietIp:   ctx.ClientIP(),
-				UserID:    currentUser.ID,
-				User:      currentUser,
-				InOut:     "in",
-				LoginedAt: &t,
+		if auth.Metod == "password" {
+			if !db.DB.Where("password = ?", auth.Password).First(&currentUser).RecordNotFound() {
+				// t := time.NowLoginAt: time.Now,
+				t := time.Now()
+				login := models.LogLogin{
+					ClietIp:   ctx.ClientIP(),
+					UserID:    currentUser.ID,
+					User:      currentUser,
+					InOut:     "in",
+					LoginedAt: &t,
+					Device:    "terminal",
+				}
+				// fmt.Println(login)
+				if err := db.DB.Create(&login).Error; err != nil {
+					fmt.Println(err)
+				}
+				user := models.UserApi{
+					ID:     currentUser.ID,
+					Name:   currentUser.Name,
+					Email:  currentUser.Email,
+					Gender: currentUser.Gender,
+					Role:   currentUser.Role,
+					Token:  "",
+				}
+				ctx.JSON(http.StatusOK, &user)
+			} else {
+				ctx.JSON(http.StatusNotFound, gin.H{"status": "error", "message": "User not found"})
 			}
-			// fmt.Println(login)
-			if err := db.DB.Create(&login).Error; err != nil {
-				fmt.Println(err)
-			}
-			user := models.UserApi{
-				ID:     currentUser.ID,
-				Name:   currentUser.Name,
-				Email:  currentUser.Email,
-				Gender: currentUser.Gender,
-				Role:   currentUser.Role,
-			}
-			ctx.JSON(http.StatusOK, &user)
 		} else {
-			ctx.JSON(http.StatusNotFound, gin.H{"status": "error", "message": "User not found"})
+			ctx.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Bad request metod"})
 		}
 	} else {
 		ctx.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Bad request"})
 	}
-
 }
 
 func LogoutApi(ctx *gin.Context) {
