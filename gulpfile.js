55f19680cbf48019680f01430e0df2c0e43581fb
jason weng <wengjianm@gmail.com>
2016-03-31 18:39:29 +0800

    style(frontend) add product details layout @1.4

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+
+function adminTasks() {
+  var pathto = function (file) {
+        return ('./public/' + file);
+      };
+  var scripts = {
+        src: pathto('javascripts/*.js'),
+        dest: pathto('dist')
+      };
+  var styles = {
+        src: pathto('stylesheets/qor.scss'),
+        scss: pathto('stylesheets/**/*.scss'),
+        dest: pathto('dist')
+      }
+
+  gulp.task('js', function () {
+    return gulp.src(scripts.src)
+    .pipe(plugins.concat('app.js'))
+    .pipe(plugins.uglify())
+    .pipe(gulp.dest(scripts.dest));
+  });
+
+  gulp.task('sass', function () {
+    return gulp.src(styles.src)
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass())
+    .pipe(plugins.csscomb())
+    .pipe(plugins.minifyCss())
+    .pipe(plugins.sourcemaps.write('./'))
+    .pipe(gulp.dest(styles.dest));
+  });
+
+  gulp.task('watch', function () {
+    gulp.watch(scripts.src, ['js']);
+    gulp.watch(styles.scss, ['sass']);
+  });
+
+  gulp.task('default', ['watch']);
+}
+
+
+// Init
+// -----------------------------------------------------------------------------
+
+console.log('Running "qor-example" module task...');
+adminTasks();
+
+
+// Task for compress js vendor assets
+gulp.task('compressJavaScriptVendor', function () {
+  return gulp.src(['!./public/vendors/jquery.js','./public/vendors/*.js'])
+  .pipe(plugins.concat('vendors.js'))
+  .pipe(gulp.dest('./public/dist'));
+});
--- public/javascripts/app.js
+++
@@ -1,1 +0,0 @@
-require('../stylesheets/qor.scss');
