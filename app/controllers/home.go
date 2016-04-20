d62eeb0492154d1988045947ec0f225f2daf3c2a
binku <binku87@gmail.com>
2016-04-20 11:43:19 +0800

    inline-edit: using another way to do inline edit

--- app/controllers/home.go
+++ app/controllers/home.go
@@ -2,12 +2,13 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/qor/i18n/inline_edit"
 	"github.com/qor/qor-example/app/models"
 	"github.com/qor/qor-example/config"
 	"github.com/qor/qor-example/config/admin"
+	"github.com/qor/qor-example/config/i18n"
 	"github.com/qor/qor-example/db"
 	"github.com/qor/seo"
-
 	"github.com/qor/widget"
 )
 
@@ -17,8 +18,9 @@ func HomeIndex(ctx *gin.Context) {
 	seoObj := models.SEOSetting{}
 	db.DB.First(&seoObj)
 
+	i18nFuncMap := inline_edit.GenerateFuncMaps(i18n.I18n, "en-US", nil)
 	widgetContext := widget.NewContext(map[string]interface{}{})
-	config.View.Execute(
+	config.View.Funcs(i18nFuncMap).Execute(
 		"home_index",
 		gin.H{
 			"SeoTag":           seoObj.HomePage.Render(seoObj, nil),
