a981df5f4cfb5fc16e9b16bf2691a3872b6edcd8
binku <binku87@gmail.com>
2016-06-24 16:45:41 +0800

    Add validation for user model

--- app/models/user.go
+++ app/models/user.go
@@ -1,15 +1,16 @@
 package models
 
 import (
+	"github.com/asaskevich/govalidator"
 	"github.com/jinzhu/gorm"
 	"time"
 )
 
 type User struct {
 	gorm.Model
-	Email     string
-	Password  string
-	Name      string
+	Email     string `valid:"required,email,uniqEmail~Email already be token"`
+	Password  string `valid:"password~Password can't be blank"`
+	Name      string `valid:"required"`
 	Gender    string
 	Role      string
 	Addresses []Address
@@ -21,6 +22,24 @@ type User struct {
 	// Recover
 	RecoverToken       string
 	RecoverTokenExpiry time.Time
+}
+
+func (user *User) Validate(db *gorm.DB) {
+	govalidator.CustomTypeTagMap.Set("uniqEmail", govalidator.CustomTypeValidator(func(email interface{}, context interface{}) bool {
+		var user User
+		db.First(&user, "email = ?", email)
+		if user.ID == 0 || user.ID == context.(User).ID {
+			return true
+		}
+		return false
+	}))
+
+	govalidator.CustomTypeTagMap.Set("password", govalidator.CustomTypeValidator(func(email interface{}, context interface{}) bool {
+		if context.(User).ID == 0 && context.(User).Password == "" {
+			return false
+		}
+		return true
+	}))
 }
 
 func (user User) DisplayName() string {
