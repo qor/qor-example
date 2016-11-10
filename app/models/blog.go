7257a8c0978aa02f7807d7a68ea8880681b5a42c
Jinzhu <wosmvp@gmail.com>
2016-11-10 17:14:14 +0800

    Add publish Version, Schedule, Visible to Blog

--- app/models/blog.go
+++ app/models/blog.go
@@ -3,6 +3,7 @@ package models
 import (
 	"github.com/jinzhu/gorm"
 	"github.com/qor/accessibility/hyperlink"
+	"github.com/qor/publish2"
 )
 
 type Article struct {
@@ -12,4 +13,7 @@ type Article struct {
 	Title    string
 	Content  string `gorm:"type:text"`
 	FromURL  hyperlink.HyperLink
+	publish2.Version
+	publish2.Schedule
+	publish2.Visible
 }
