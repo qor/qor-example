ae6d5d4a94088585210980bfb447b925211b37b4
binku <binku87@gmail.com>
2016-12-28 15:14:12 +0800

    show edit product button on actionbar

--- app/controllers/product.go
+++ app/controllers/product.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/qor/action_bar"
 	"github.com/qor/qor-example/app/models"
 	"github.com/qor/qor-example/config"
 	"github.com/qor/qor-example/config/admin"
@@ -33,10 +34,11 @@ func ProductShow(ctx *gin.Context) {
 	DB(ctx).Preload("Product").Preload("Color").Preload("SizeVariations.Size").Where(&models.ColorVariation{ProductID: product.ID, ColorCode: colorCode}).First(&colorVariation)
 	DB(ctx).First(&seoSetting)
 
+	editButton := admin.ActionBar.RenderEditButton(ctx.Writer, ctx.Request, product)
 	config.View.Funcs(funcsMap(ctx)).Execute(
 		"product_show",
 		gin.H{
-			"ActionBarTag":   admin.ActionBar.Render(ctx.Writer, ctx.Request),
+			"ActionBarTag":   admin.ActionBar.Render(ctx.Writer, ctx.Request, action_bar.Config{InlineActions: []template.HTML{editButton}}),
 			"Product":        product,
 			"ColorVariation": colorVariation,
 			"SeoTag":         seoSetting.ProductPage.Render(seoSetting, product),
