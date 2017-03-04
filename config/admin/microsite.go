e2acc82e385aa13eb0473fe27e3075080c86ac88
Jinzhu <wosmvp@gmail.com>
2017-03-04 15:08:31 +0800

    Add AWS Manager config

--- config/admin/microsite.go
+++ config/admin/microsite.go
@@ -4,6 +4,8 @@ package admin
 
 import (
 	"enterprise.getqor.com/microsite"
+	"enterprise.getqor.com/microsite/develop/aws_manager"
+	"github.com/jinzhu/configor"
 	"github.com/qor/admin"
 	"github.com/qor/qor-example/config"
 )
@@ -14,10 +16,25 @@ type QorMicroSite struct {
 	microsite.QorMicroSite
 }
 
+type AWSManagerConfig struct {
+	AccessID  string `env:"AWS_ACCESS_KEY_ID" required:"true"`
+	AccessKey string `env:"AWS_SECRET_ACCESS_KEY" required:"true"`
+	Region    string `env:"AWS_Region" required:"true"`
+	Bucket    string `env:"AWS_Bucket" required:"true"`
+}
+
 func init() {
 	initWidgets()
+	awsConfig := AWSManagerConfig{}
+	configor.Load(awsConfig)
 
-	MicroSite = microsite.New(&microsite.Config{Dir: config.Root + "/public/microsites", Widgets: Widgets})
+	MicroSite = microsite.New(&microsite.Config{Dir: config.Root + "/public/microsites", Widgets: Widgets, DevelopManager: aws_manager.New(&aws_manager.Config{
+		AccessID:  awsConfig.AccessID,
+		AccessKey: awsConfig.AccessKey,
+		Region:    awsConfig.Region,
+		Bucket:    awsConfig.Bucket,
+	})})
+
 	MicroSite.Resource = Admin.AddResource(&QorMicroSite{}, &admin.Config{Name: "MicroSite"})
 
 	Admin.AddResource(MicroSite)
