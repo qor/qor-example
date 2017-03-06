e65bc3d7edaec4888d6a1694c08e15e97d198a18
Jinzhu <wosmvp@gmail.com>
2017-03-06 19:57:20 +0800

    remove qor microsite widget setting

--- db/migrations/enterprise_migration.go
+++ db/migrations/enterprise_migration.go
@@ -3,10 +3,9 @@
 package migrations
 
 import (
-	"enterprise.getqor.com/microsite"
 	"github.com/qor/qor-example/config/admin"
 )
 
 func init() {
-	AutoMigrate(&admin.QorMicroSite{}, &microsite.QorMicorSiteWidgetSetting{})
+	AutoMigrate(&admin.QorMicroSite{})
 }
