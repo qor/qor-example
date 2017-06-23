bf404c5fc61ad90e18b025b467b4c629ed1642b3
Jinzhu <wosmvp@gmail.com>
2017-06-23 14:11:20 +0800

    Refactor home index controller

--- app/controllers/home.go
+++ app/controllers/home.go
@@ -4,29 +4,13 @@ import (
 	"net/http"
 
 	"github.com/qor/qor"
-	apputils "github.com/qor/qor-example/config/utils"
 	"github.com/qor/qor/utils"
 
-	"github.com/qor/qor-example/app/models"
 	"github.com/qor/qor-example/config"
 )
 
 func HomeIndex(w http.ResponseWriter, req *http.Request) {
-	var (
-		tx       = apputils.GetDB(req)
-		products []models.Product
-	)
-
-	tx.Limit(9).Preload("ColorVariations").Find(&products)
-
-	config.View.Execute(
-		"home_index",
-		map[string]interface{}{
-			"Products": products,
-		},
-		req,
-		w,
-	)
+	config.View.Execute("home_index", map[string]interface{}{}, req, w)
 }
 
 func SwitchLocale(w http.ResponseWriter, req *http.Request) {
