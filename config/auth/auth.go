fdbd683060d862f3f904ce4a9a5854348fa9470d
Jinzhu <wosmvp@gmail.com>
2017-07-05 21:05:38 +0800

    Add clean theme

--- config/auth/auth.go
+++ config/auth/auth.go
@@ -2,10 +2,7 @@ package auth
 
 import (
 	"github.com/qor/auth"
-	"github.com/qor/auth/database"
-	"github.com/qor/auth/oauth/github"
-	"github.com/qor/auth/oauth/google"
-	"github.com/qor/auth/phone"
+	"github.com/qor/auth_themes/clean"
 	"github.com/qor/qor-example/app/models"
 	"github.com/qor/qor-example/config"
 	"github.com/qor/qor-example/db"
@@ -18,8 +15,5 @@ var Auth = auth.New(&auth.Config{
 })
 
 func init() {
-	Auth.RegisterProvider(database.New(nil))
-	Auth.RegisterProvider(phone.New())
-	Auth.RegisterProvider(github.New(&config.Config.Github))
-	Auth.RegisterProvider(google.New(&config.Config.Google))
+	clean.New(Auth)
 }
