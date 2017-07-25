18eff72fc2be333efe242aa755b6bbec3d65e0d5
Jinzhu <wosmvp@gmail.com>
2017-07-25 18:29:52 +0800

    Add default mailer

--- config/auth/auth.go
+++ config/auth/auth.go
@@ -1,11 +1,19 @@
 package auth
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+
 	"github.com/qor/auth"
 	"github.com/qor/auth_themes/clean"
+	"github.com/qor/mailer"
+	"github.com/qor/mailer/gomailer"
 	"github.com/qor/qor-example/app/models"
 	"github.com/qor/qor-example/config"
 	"github.com/qor/qor-example/db"
+	gomail "gopkg.in/gomail.v2"
 )
 
 // Auth initialize auth
@@ -14,3 +22,18 @@ var Auth = clean.New(&auth.Config{
 	Render:    config.View,
 	UserModel: models.User{},
 })
+
+func init() {
+	sender := gomail.SendFunc(gomail.SendFunc(func(from string, to []string, msg io.WriterTo) error {
+		var buf bytes.Buffer
+		fmt.Printf("From: %v\n", from)
+		fmt.Printf("To: %v\n", strings.Join(to, ", "))
+		msg.WriteTo(&buf)
+		fmt.Println(buf.String())
+		return nil
+	}))
+
+	Auth.Config.Mailer = mailer.New(&mailer.Config{
+		Sender: gomailer.New(&gomailer.Config{Sender: sender}),
+	})
+}
