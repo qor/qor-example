70684dfb1b3971168df933877c603654232fee19
Jinzhu <wosmvp@gmail.com>
2017-07-27 18:13:48 +0800

    Use default providers

--- config/auth/auth.go
+++ config/auth/auth.go
@@ -2,16 +2,28 @@ package auth
 
 import (
 	"github.com/qor/auth"
-	"github.com/qor/auth_themes/clean"
+	"github.com/qor/auth/database"
+	"github.com/qor/auth/oauth/github"
+	"github.com/qor/auth/oauth/google"
+	"github.com/qor/auth/phone"
 	"github.com/qor/qor-example/app/models"
 	"github.com/qor/qor-example/config"
 	"github.com/qor/qor-example/db"
 )
 
 // Auth initialize auth
-var Auth = clean.New(&auth.Config{
+var Auth = auth.New(&auth.Config{
 	DB:        db.DB,
 	Render:    config.View,
 	Mailer:    config.Mailer,
 	UserModel: models.User{},
 })
+
+func init() {
+	Auth.RegisterProvider(database.New(&database.Config{
+		Confirmable: true,
+	}))
+	Auth.RegisterProvider(phone.New())
+	Auth.RegisterProvider(github.New(&config.Config.Github))
+	Auth.RegisterProvider(google.New(&config.Config.Google))
+}
