c877f6296f4e09d0f28f7a056b0333d1782ae17f
Jinzhu <wosmvp@gmail.com>
2017-09-20 10:49:54 +0800

    Auth Theme with multiple providers

--- config/auth/auth.go
+++ config/auth/auth.go
@@ -8,39 +8,22 @@ import (
 	"github.com/qor/auth/providers/facebook"
 	"github.com/qor/auth/providers/github"
 	"github.com/qor/auth/providers/google"
-	"github.com/qor/auth/providers/password"
-	"github.com/qor/auth/providers/phone"
 	"github.com/qor/auth/providers/twitter"
+	"github.com/qor/auth_themes/clean"
 	"github.com/qor/qor-example/app/models"
 	"github.com/qor/qor-example/config"
 	"github.com/qor/qor-example/db"
 )
 
-var Auth = auth.New(&auth.Config{
-	DB:        db.DB,
-	Render:    config.View,
-	Mailer:    config.Mailer,
-	UserModel: models.User{},
-})
-
-func init() {
-	Auth.RegisterProvider(password.New(&password.Config{Confirmable: true}))
-	Auth.RegisterProvider(phone.New())
-	Auth.RegisterProvider(github.New(&config.Config.Github))
-	Auth.RegisterProvider(google.New(&config.Config.Google))
-	Auth.RegisterProvider(facebook.New(&config.Config.Facebook))
-	Auth.RegisterProvider(twitter.New(&config.Config.Twitter))
-}
-
 var (
 	// Auth initialize Auth for Authentication
-	// Auth = clean.New(&auth.Config{
-	// 	DB:         db.DB,
-	// 	Render:     config.View,
-	// 	Mailer:     config.Mailer,
-	// 	UserModel:  models.User{},
-	// 	Redirector: auth.Redirector{RedirectBack: config.RedirectBack},
-	// })
+	Auth = clean.New(&auth.Config{
+		DB:         db.DB,
+		Render:     config.View,
+		Mailer:     config.Mailer,
+		UserModel:  models.User{},
+		Redirector: auth.Redirector{RedirectBack: config.RedirectBack},
+	})
 
 	// Authority initialize Authority for Authorization
 	Authority = authority.New(&authority.Config{
@@ -49,5 +32,10 @@ var (
 )
 
 func init() {
+	Auth.RegisterProvider(github.New(&config.Config.Github))
+	Auth.RegisterProvider(google.New(&config.Config.Google))
+	Auth.RegisterProvider(facebook.New(&config.Config.Facebook))
+	Auth.RegisterProvider(twitter.New(&config.Config.Twitter))
+
 	Authority.Register("logged_in_half_hour", authority.Rule{TimeoutSinceLastLogin: time.Minute * 30})
 }
