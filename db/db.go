ae24085d62a1e2f75c15312794b3b61b3726ed95
Charles Shen <shenzhaoyan@gmail.com>
2017-10-13 17:54:37 +0800

    Set MySQL character set to UTF-8

--- db/db.go
+++ db/db.go
@@ -28,6 +28,7 @@ func init() {
 	dbConfig := config.Config.DB
 	if config.Config.DB.Adapter == "mysql" {
 		DB, err = gorm.Open("mysql", fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?parseTime=True&loc=Local", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Name))
+		DB = DB.Set("gorm:table_options", "CHARSET=utf8")
 	} else if config.Config.DB.Adapter == "postgres" {
 		DB, err = gorm.Open("postgres", fmt.Sprintf("postgres://%v:%v@%v/%v?sslmode=disable", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Name))
 	} else if config.Config.DB.Adapter == "sqlite" {
