52a3b7db7db5cb451157876ebf521356ae82590e
Jinzhu <wosmvp@gmail.com>
2017-12-19 10:47:40 +0800

    Add utils

--- app/home/home.go
+++ app/home/home.go
@@ -2,6 +2,7 @@ package home
 
 import (
 	"github.com/qor/qor-example/config/application"
+	"github.com/qor/qor-example/utils"
 	"github.com/qor/render"
 )
 
@@ -23,5 +24,6 @@ type Config struct {
 func (App) ConfigureApplication(application *application.Application) {
 	controller := &Controller{View: render.New(&render.Config{AssetFileSystem: application.AssetFS.NameSpace("home")}, "app/home/views")}
 
+	utils.AddFuncMapMaker(controller.View)
 	application.Router.Get("/", controller.Index)
 }
