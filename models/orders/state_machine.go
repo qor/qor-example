d6893522428b3f024e07a630864a34bc2592a521
Jinzhu <wosmvp@gmail.com>
2018-03-01 19:46:19 +0800

    Freeze order prices after checkout

--- models/orders/state_machine.go
+++ models/orders/state_machine.go
@@ -82,9 +82,19 @@ func init() {
 
 	OrderState.Event("checkout").To("pending").From("draft").Before(func(value interface{}, tx *gorm.DB) error {
 		order := value.(*Order)
+		tx.Model(order).Association("OrderItems").Find(&order.OrderItems)
 		refAttrs, err := config.AmazonPay.SetOrderReferenceDetails(order.OrderReferenceID, amazonpay.OrderReferenceAttributes{
 			OrderTotal: amazonpay.OrderTotal{CurrencyCode: "JPY", Amount: utils.FormatPrice(order.Amount())},
 		})
+
+		if err == nil {
+			for _, orderItem := range order.OrderItems {
+				orderItem.Price = orderItem.SellingPrice()
+			}
+			order.PaymentAmount = order.Amount()
+			order.PaymentTotal = order.Total()
+		}
+
 		result, _ := json.Marshal(refAttrs)
 		order.PaymentLog += "\n" + string(result)
 		return err
