45b63eae8befecc31ff046d4f0dd52b4956d0893
Jinzhu <wosmvp@gmail.com>
2018-05-02 17:12:14 +0800

    Add Reports

--- app/reports/reports.go
+++ app/reports/reports.go
@@ -1,6 +1,13 @@
 package reports
 
-import "github.com/qor/qor-example/config/application"
+import (
+	"github.com/qor/admin"
+	"github.com/qor/metas/daterange"
+	"github.com/qor/metas/frequency"
+	"github.com/qor/notification"
+	"github.com/qor/qor-example/config/application"
+	"github.com/qor/worker"
+)
 
 // New new home app
 func New(config *Config) *App {
@@ -18,4 +25,22 @@ type Config struct {
 
 // ConfigureApplication configure application
 func (app App) ConfigureApplication(application *application.Application) {
+	Worker := worker.New()
+
+	type kpiReport struct {
+		Template string
+		daterange.DateRange
+		frequency.Frequency
+		notification.Notification
+	}
+
+	Worker.RegisterJob(&worker.Job{
+		Name: "KPI Report",
+		Handler: func(argument interface{}, qorJob worker.QorJobInterface) error {
+			return nil
+		},
+		Resource: application.Admin.NewResource(&kpiReport{}),
+	})
+
+	application.Admin.AddResource(Worker, &admin.Config{Menu: []string{"Report Management", "KPI Report"}})
 }
